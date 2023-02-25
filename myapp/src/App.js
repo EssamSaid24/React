04e40aee31a9d72b4739b5300825ed3ca61850a4
Essam Said <100171793+EssamSaid24@users.noreply.github.com>
2023-02-25 23:35:23 +0200

    convert to React

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,32 @@
 
 function App() {
   return (
-<section>
-      <h1 className ="title">Essam</h1>
-  
-</section>  
+<>
+  <h1>To Do List</h1>
+  <section id="container">
+    <div className="task">
+      <span className="icon-star-full icon" />
+      <p>Task 1</p>
+      <div>
+        <span className="icon-cross icon" />
+        <span className="icon-checkmark icon" />
+      </div>
+    </div>
+    <div className="task">
+      <span className="icon-star-full icon" />
+      <p>Task 2</p>
+      <div>
+        <span className="icon-cross icon" />
+        <span className="icon-checkmark icon" />
+      </div>
+    </div>
+  </section>
+  <form>
+    <h2>New Task</h2>
+    <input type="text" placeholder="what is your next task?" />
+    <button id="btn" className="icon-arrow-up icon" />
+  </form>
+</>
 );
 }
 
